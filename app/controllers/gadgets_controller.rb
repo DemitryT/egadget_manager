b499a4c5c1cdeb68b43603b75cf3e33819a47c04
Demitry Toumilovich <demitryt@gmail.com>
2014-01-10 14:00:20 -0500

    Add ability to add, edit, list and delete gadgets for all signed in users

--- app/controllers/gadgets_controller.rb
+++ app/controllers/gadgets_controller.rb
@@ -1,16 +1,33 @@
 class GadgetsController < ApplicationController
   def index
+    @all_gadgets = Gadget.where(user_id: current_user.id) if current_user
   end
 
   def show
+    @gadget = Gadget.find(params[:id])
+    redirect_to action: :index if @gadget.nil?
   end
 
   def new
+    @gadget = Gadget.new
   end
 
   def create
+    @gadget = Gadget.create(params[:gadget])
+    flash[:notice] = 'Your gadget has been saved' if @gadget.save
+    render :new
+  end
+
+  def edit
+    @gadget = Gadget.find(params[:id])
   end
 
   def update
+    @gadget = Gadget.find(params[:id])
+    if @gadget.update_attributes(params[:gadget])
+      redirect_to(@gadget)
+    else
+      render "edit"
+    end
   end
 end
